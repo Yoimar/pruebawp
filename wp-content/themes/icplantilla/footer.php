56f1967fe26101b27e02b2f66fa623d608f6d171
Yoimar <urbina.yoimar@gmail.com>
2017-05-16 07:50:00 -0430

    1er commit con 6 entradas

---
+++ wp-content/themes/icplantilla/footer.php
@@ -0,0 +1,7 @@
+		<footer>
+			<small>Yoimar Urbina &copy; <?php echo date('Y'); ?></small>
+
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ wp-content/themes/icplantilla/home.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+<?php if (have_posts()): ?>
+	<section>
+		<?php while (have_posts()): the_post(); ?>
+			<article>
+				<header>
+					<h2>
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</h2>
+					<time datatime="<?php the_time('Y-m-j') ?>">
+						<?php the_time('j-m-Y') ?>
+					</time>
+				</header>
+				<?php the_excerpt(); ?>
+				<footer>
+					<address> 
+					Por <?php the_author_posts_link(); ?>
+					</address>
+				</footer>
+			</article>
+		<?php endwhile; ?>
+		<div class="pagination">
+			<span class="in-left"><?php next_posts_link('<<Entradas Antiguas'); ?></span>	
+			<span class="in-right"><?php previous_posts_link('Entradas Recientes >>'); ?></span>	
+		</div>
+	</section>
+<?php else: ?>
+	<p><?php _e('Aqui no hay nada'); ?></p>
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
